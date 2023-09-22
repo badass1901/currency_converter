bdeb526ae37d5e939895f6105a675c29c7927ad9
Shashwat Sagar <59369746+badass1901@users.noreply.github.com>
2023-09-22 18:01:23 +0530

    custom hook useCurrencyInfo is created

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState();
+
+  //we want to fetch this api automatically everytime the function is called or the value of currency is changed, hence we use useEffect hook
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
